defa32104c63a4a7e7c640f2cf3c08e88cec089d
Cristian1477 <90467759+Cristian1477@users.noreply.github.com>
2022-03-25 15:23:17 +0000

    m

---
+++ studentWork/Cristian1477/ToStringDemo.java
@@ -0,0 +1,19 @@
+public class ToStringDemo {
+
+    private String name;
+
+    public ToStringDemo(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        ToStringDemo bob = new ToStringDemo("bob");
+        System.out.println(bob);
+        System.out.println(bob.toString());
+
+        ToStringDemo alice = new ToStringDemo("alice");
+        System.out.println(alice);
+        System.out.println(alice.toString());
+    }
+    
+}
